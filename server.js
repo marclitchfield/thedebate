c72f08d2ec837984ea5e98278a8d51fc7ea53f2c
marclitchfield <marclitchfield@gmail.com>
2014-07-24 20:11:13 -0700

    Render greeting from jade

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function(req, res){
-  res.send('Welcome to the debate');
+  res.render('index', {});
 });
 
 var server = app.listen(9002, function() {
-    console.log('Listening on port %d', server.address().port);
+  console.log('Listening on port %d', server.address().port);
 });
