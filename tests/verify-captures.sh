43fc1e7bcdbb4c4cb37ec188b2cfea53e5e18f96
marclitchfield <marclitchfield@gmail.com>
2014-07-26 21:32:31 -0700

    Do screenshot check in travis config

--- tests/verify-captures.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git status | grep captures
-if [ $(git status | grep captures | wc -l | awk '{print $1}') -ne "0" ]; then
-  echo "Screenshot comparison failed. There are changes to baseline captures."
-  git status | grep captures
-  exit 1
-fi
