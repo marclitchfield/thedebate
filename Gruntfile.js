ace77cf54f0fc944402601d6c5c1a816f21dbf33
marclitchfield <marclitchfield@gmail.com>
2014-07-27 15:48:28 -0700

    Clean task to remove thumbs.db files. Windows!!

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,12 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    clean: {
+      build: {
+        src: ['tests/captures/**/Thumbs.db']
+      }
+    },
+
     jshint: {
       files: ['**/*.js', '!**/node_modules/**'],
       options: {
@@ -12,6 +18,7 @@ module.exports = function(grunt) {
 
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.registerTask('default', ['jshint']);  
