60efd476b2aff841d08c495b1ae0d6d39609ba0d
marclitchfield <marclitchfield@gmail.com>
2014-07-26 20:51:43 -0700

    Baseline screenshot

--- tests/ui-tests.js
+++ tests/ui-tests.js
@@ -3,6 +3,10 @@ casper.test.begin('The Debate homepage', 1, function suite(test) {
     test.assertSelectorHasText('.banner h1', 'The Debate');
   });
 
+  casper.then(function() {
+    this.capture('tests/captures/homepage.png');
+  })
+
   casper.run(function() {
     test.done();
   });
