41013c1fab0ecee1175ff6b3aaade27796697078
marclitchfield <marclitchfield@gmail.com>
2014-07-27 19:27:17 -0700

    Exclude actual.

--- tests/ui-tests.js
+++ tests/ui-tests.js
@@ -1,5 +1,9 @@
 casper.test.begin('The Debate homepage', 1, function suite(test) {
   casper.start("http://localhost:9002", function() {
+    this.viewport(800, 600);
+  });
+
+  casper.then(function() {
     test.assertSelectorHasText('.banner h1', 'The Debate');
     this.capture('tests/captures/actual/homepage.png');
   });
