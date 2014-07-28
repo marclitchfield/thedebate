cb89376de0bda5f8f0c10c1095299a71a0301369
marclitchfield <marclitchfield@gmail.com>
2014-07-27 19:35:45 -0700

    Change capture statement. Decrease viewport to reduce file size

--- tests/ui-tests.js
+++ tests/ui-tests.js
@@ -1,11 +1,11 @@
 casper.test.begin('The Debate homepage', 1, function suite(test) {
   casper.start("http://localhost:9002", function() {
-    this.viewport(800, 600);
+    this.viewport(420, 300);
   });
 
   casper.then(function() {
     test.assertSelectorHasText('.banner h1', 'The Debate');
-    this.capture('tests/captures/actual/homepage.png');
+    this.captureSelector('tests/captures/actual/homepage.png', 'body', { quality: 100, format: 'png' });
   });
 
   casper.run(function() {
