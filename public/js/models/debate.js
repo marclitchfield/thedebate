6aaaece9ad4aa227d0c60b1dbb51e71e1d2d0aa5
marclitchfield <marclitchfield@gmail.com>
2014-08-02 12:09:59 -0700

    Show debates on home page

--- public/js/models/debate.js
+++ public/js/models/debate.js
@@ -5,14 +5,67 @@ DebateApp.Debate = DS.Model.extend({
 DebateApp.Debate.FIXTURES = [
   {
     id: 1,
-    title: 'vi vs Emacs'
+    title: 'Lucas ipsum dolor sit amet wyl jusik geonosian elomin bane wicket tchuukthai elrood droid chadra-fan. Solo snivvian whitesun baba cracken shawda grievous.'
   },
   {
     id: 2,
-    title: 'Static vs Dynamic Typing'
+    title: 'Felth chommell hoth hutta r4-p17. Pavan thrawn boss kessel obi-wan boba tarasin anakin raynar'
   },
   {
     id: 3,
-    title: 'Corn or husk?'
+    title: 'Yuzzum aparo roan nadon shistavanen. Jamillia kastolar kota skywalker muzzer. Lama stass zhell gendai. Organa vima-da-boda cliegg fode bren ric.'
+  },
+  {
+    id: 4,
+    title: 'Ventress darth yowza gizka ubb danni boz leia. Hutt -lom darth qrygg jabba jar hallotan. Bib lars darth gamorrean aruzan ybith.'
+  },
+  {
+    id: 5,
+    title: 'Lucas ipsum dolor sit amet wyl jusik geonosian elomin bane wicket tchuukthai elrood droid chadra-fan. Solo snivvian whitesun baba cracken shawda grievous.'
+  },
+  {
+    id: 6,
+    title: 'Felth chommell hoth hutta r4-p17. Pavan thrawn boss kessel obi-wan boba tarasin anakin raynar'
+  },
+  {
+    id: 7,
+    title: 'Yuzzum aparo roan nadon shistavanen. Jamillia kastolar kota skywalker muzzer. Lama stass zhell gendai. Organa vima-da-boda cliegg fode bren ric.'
+  },
+  {
+    id: 8,
+    title: 'Ventress darth yowza gizka ubb danni boz leia. Hutt -lom darth qrygg jabba jar hallotan. Bib lars darth gamorrean aruzan ybith.'
+  },
+  {
+    id: 9,
+    title: 'Lucas ipsum dolor sit amet wyl jusik geonosian elomin bane wicket tchuukthai elrood droid chadra-fan. Solo snivvian whitesun baba cracken shawda grievous.'
+  },
+  {
+    id: 10,
+    title: 'Felth chommell hoth hutta r4-p17. Pavan thrawn boss kessel obi-wan boba tarasin anakin raynar'
+  },
+  {
+    id: 11,
+    title: 'Yuzzum aparo roan nadon shistavanen. Jamillia kastolar kota skywalker muzzer. Lama stass zhell gendai. Organa vima-da-boda cliegg fode bren ric.'
+  },
+  {
+    id: 12,
+    title: 'Ventress darth yowza gizka ubb danni boz leia. Hutt -lom darth qrygg jabba jar hallotan. Bib lars darth gamorrean aruzan ybith.'
+  },
+  {
+    id: 13,
+    title: 'Lucas ipsum dolor sit amet wyl jusik geonosian elomin bane wicket tchuukthai elrood droid chadra-fan. Solo snivvian whitesun baba cracken shawda grievous.'
+  },
+  {
+    id: 14,
+    title: 'Felth chommell hoth hutta r4-p17. Pavan thrawn boss kessel obi-wan boba tarasin anakin raynar'
+  },
+  {
+    id: 15,
+    title: 'Yuzzum aparo roan nadon shistavanen. Jamillia kastolar kota skywalker muzzer. Lama stass zhell gendai. Organa vima-da-boda cliegg fode bren ric.'
+  },
+  {
+    id: 16,
+    title: 'Ventress darth yowza gizka ubb danni boz leia. Hutt -lom darth qrygg jabba jar hallotan. Bib lars darth gamorrean aruzan ybith.'
   }
+
 ];
