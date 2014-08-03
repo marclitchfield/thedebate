74a9e7b3e6e450f7f7e69ae6544554c43c1ea90d
marclitchfield <marclitchfield@gmail.com>
2014-08-02 23:04:08 -0700

    Minor fuss

--- public/js/router.js
+++ public/js/router.js
@@ -1,5 +1,5 @@
 App.Router.map(function() {
-  this.resource('debates', { path: '/' }, function() { });
+  this.resource('debates', { path: '/' }, function() {});
   this.resource('debate', { path: '/debate/:debate_id' });
 });
 
