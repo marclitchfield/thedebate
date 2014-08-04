b301d655dc07c460708395bce5a8cd7037074a73
marclitchfield <marclitchfield@gmail.com>
2014-08-03 22:37:44 -0700

    Introduce LESS. Grunt now compiles LESS->CSS, and Jade->HTML.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,28 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    jade: {
+      default: {
+        options: {
+          data: {
+            debug: false
+          }
+        },
+        files: {
+          'public/index.html': 'views/index.jade'
+        }
+      }
+    },
+
+    less: {
+      default: {
+        options: {
+          cleancss: true
+        },
+        files: {
+          'public/css/main.css': 'styles/main.less'
+        }
+      }
+    },
 
     jshint: {
       files: ['**/*.js', '!**/node_modules/**', '!public/js/lib/**'],
@@ -8,11 +30,24 @@ module.exports = function(grunt) {
         globals: {
         }
       }
+    },
+
+    watch: {
+      scripts: {
+        files: ['views/**/*.jade', 'styles/**/*.less', 'tests/**/*.js', 'public/js/**/*.js', '!public/js/lib/**'],
+        tasks: ['jade','less','jshint'],
+        options: {
+          spawn: false
+        },
+      }
     }
 
   });
 
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-exec');
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint','jade','less']);
 };
