017e72ab3b183e6735abe9a758272161507b57e3
marclitchfield <marclitchfield@gmail.com>
2014-08-10 08:09:04 -0700

    Got controllers working. All ui-tests pass

---
+++ app/controllers/debate.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+  actions: {
+    createStatement: function() {
+      var body = this.get('newBody');
+      if (!body) { return false; }
+      if (!body.trim()) { return; }
+
+      var statement = this.store.createRecord('statement', {
+        body: body,
+        score: 0,
+        support: 0,
+        opposition: 0
+      });
+
+      this.set('newBody', '');
+      this.get('statements').pushObject(statement);
+      statement.save();
+    }
+  }  
+});
