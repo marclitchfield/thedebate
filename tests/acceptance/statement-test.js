ecf8b45331bb4de866c6ddd373e71b844b902921
marclitchfield <marclitchfield@gmail.com>
2014-08-13 20:42:57 -0700

    Split tests for each page into separate modules. It didn't help stability

--- tests/acceptance/statement-test.js
+++ tests/acceptance/statement-test.js
@@ -1,76 +1,16 @@
 import Ember from 'ember';
 import startApp from '../helpers/start-app';
-import debateIndexPage from '../acceptance/pages/debates/index';
-import debatePage from '../acceptance/pages/debate';
-import statementPage from '../acceptance/pages/statement';
+import statementPage from '../helpers/pages/statement';
 
 var App;
 
-module('Acceptance: UI Tests', {
+module('Acceptance: Statement Page Tests', {
   setup: function() {
     App = startApp();
   },
   teardown: function() {
     Ember.run(App, 'destroy');
   }
-});
-
-test('Visit homepage', function() {
-  Ember.run(function() {
-    visit('/');
-    andThen(function() {
-      equal(find('.banner h1').text(), 'The Debate');
-    });
-  });
-});
-
-// test('Add a new debate', function() {
-//   visit('/');
-//   andThen(function() {
-//     var debateTitle = 'New debate added by capser';
-//     fillIn('#new-debate', debateTitle);
-//     keyEvent('#new-debate', 'keypress', 13);
-//     andThen(function() {
-//       debateIndexPage.assertLast(debateTitle, '0');
-//     });
-//   });
-// });
-
-test('Navigate to debate details', function() {
-  visit('/');
-  andThen(function() {
-    var debate = debateIndexPage.first();
-    debateIndexPage.visitFirstDebate();
-    andThen(function() {
-      debatePage.assertCurrent(debate.title, debate.score);
-      debatePage.assertHasStatements();
-    });
-  });
-});
-
-test('Submit statement', function() {
-  var statementBody = 'New statement added by casper';
-  visit('/debate/1');
-  andThen(function() {
-    debatePage.submitStatement(statementBody);
-  });
-  andThen(function() {
-    debatePage.assertLastStatement(statementBody, '0');
-  });
-});
-
-test('Navigate to statement details', function() {
-  visit('/debate/1');
-  andThen(function() {
-    var debate = debatePage.current();
-    var statement = debatePage.firstStatement();
-    debatePage.visitFirstStatement();
-    andThen(function() {
-      statementPage.assertDebate(debate.title, debate.score);
-      statementPage.assertCurrent(statement.body, statement.score);
-      statementPage.assertHasResponses();
-    });  
-  });
 });
 
 test('Navigate to response details', function() {
