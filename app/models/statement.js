89ffb2f6239ae6f5fb30cb56efed7ab100391860
marclitchfield <marclitchfield@gmail.com>
2014-08-24 13:22:42 -0700

    Fix failing accceptance test by moving fixtures to separate modules.
    This fix ensures fixture data is reloaded between tests and separates
    the fixture data from the model definition. It requires an initializer
    which needs to be maintained as fixtures are added, so an improvement
    would be to dynamically invoke the 'create' method on each of the
    fixtures.

--- app/models/statement.js
+++ app/models/statement.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
-import Debate from '../models/debate';
 
-var Statement = DS.Model.extend({
+export default DS.Model.extend({
   body: DS.attr('string'),
   score: DS.attr('number'),
   support: DS.attr('number'),
@@ -27,54 +26,3 @@ var Statement = DS.Model.extend({
 function widthStyle(score, multiplier) {
     return 'width:' + multiplier * Math.log((score || 0) + 1) + '%;';
 }
-
-var fixtures = [];
-
-var statements = [
-  'Lucas ipsum dolor sit amet wyl jusik geonosian elomin bane wicket tchuukthai elrood droid chadra-fan. Solo snivvian whitesun baba cracken shawda grievous. Felth chommell hoth hutta r4-p17. Pavan thrawn boss kessel obi-wan boba tarasin anakin raynar',
-  'Felth chommell hoth hutta r4-p17. Pavan thrawn boss kessel obi-wan boba tarasin anakin raynar Yuzzum aparo roan nadon shistavanen. Jamillia kastolar kota skywalker muzzer. Lama stass zhell gendai. Organa vima-da-boda cliegg fode bren ric. Ventress darth yowza gizka ubb danni boz leia. Hutt -lom darth qrygg jabba jar hallotan. Bib lars darth gamorrean aruzan ybith.',
-  'Yuzzum aparo roan nadon shistavanen. Jamillia kastolar kota skywalker muzzer. Lama stass zhell gendai. Organa vima-da-boda cliegg fode bren ric.',
-  'Ventress darth yowza gizka ubb danni boz leia. Hutt -lom darth qrygg jabba jar hallotan. Bib lars darth gamorrean aruzan ybith.',
-  'Dengar antonio calrissian kubaz obi-wan mon shistavanen antilles. Hissa senesca draethos draethos. Maarek shawda valorum shadda askajian gunray whiphid kubaz. Felth chommell hoth hutta r4-p17. Pavan thrawn boss kessel obi-wan boba tarasin anakin raynar Yuzzum aparo roan nadon shistavanen. Jamillia kastolar kota skywalker muzzer. Lama stass zhell gendai. Organa vima-da-boda cliegg fode bren ric. Ventress darth yowza gizka ubb danni boz leia. Hutt -lom darth qrygg jabba jar hallotan. Bib lars darth gamorrean aruzan ybith.',
-  'Lucas ipsum dolor sit amet wyl jusik geonosian elomin bane wicket tchuukthai elrood droid chadra-fan. Solo snivvian whitesun baba cracken shawda grievous. Dengar antonio calrissian kubaz obi-wan mon shistavanen antilles. Hissa senesca draethos draethos. Maarek shawda valorum shadda askajian gunray whiphid kubaz. Felth chommell hoth hutta r4-p17. Pavan thrawn boss kessel obi-wan boba tarasin anakin raynar Yuzzum aparo roan nadon shistavanen. Jamillia kastolar kota skywalker muzzer. Lama stass zhell gendai. Organa vima-da-boda cliegg fode bren ric. Ventress darth yowza gizka ubb danni boz leia. Hutt -lom darth qrygg jabba jar hallotan. Bib lars darth gamorrean aruzan ybith.',
-  'Felth chommell hoth hutta r4-p17. Pavan thrawn boss kessel obi-wan boba tarasin anakin raynar Yuzzum aparo roan nadon shistavanen. Jamillia kastolar kota skywalker muzzer. Lama stass zhell gendai. Organa vima-da-boda cliegg fode bren ric. Lucas ipsum dolor sit amet wyl jusik geonosian elomin bane wicket tchuukthai elrood droid chadra-fan. Solo snivvian whitesun baba cracken shawda grievous. Dengar antonio calrissian kubaz obi-wan mon shistavanen antilles. Hissa senesca draethos draethos. Maarek shawda valorum shadda askajian gunray whiphid kubaz. Felth chommell hoth hutta r4-p17. Pavan thrawn boss kessel obi-wan boba tarasin anakin raynar Yuzzum aparo roan nadon shistavanen. Jamillia kastolar kota skywalker muzzer. Lama stass zhell gendai. Organa vima-da-boda cliegg fode bren ric. Ventress darth yowza gizka ubb danni boz leia. Hutt -lom darth qrygg jabba jar hallotan. Bib lars darth gamorrean aruzan ybith.',
-  'Yuzzum aparo roan nadon shistavanen. Jamillia kastolar kota skywalker muzzer. Lama stass zhell gendai. Organa vima-da-boda cliegg fode bren ric. Lucas ipsum dolor sit amet wyl jusik geonosian elomin bane wicket tchuukthai elrood droid chadra-fan. Solo snivvian whitesun baba cracken shawda grievous. Dengar antonio calrissian kubaz obi-wan mon shistavanen antilles. Hissa senesca draethos draethos. Maarek shawda valorum shadda askajian gunray whiphid kubaz. Felth chommell hoth hutta r4-p17. Pavan thrawn boss kessel obi-wan boba tarasin anakin raynar Yuzzum aparo roan nadon shistavanen. Jamillia kastolar kota skywalker muzzer. Lama stass zhell gendai. Organa vima-da-boda cliegg fode bren ric. Ventress darth yowza gizka ubb danni boz leia. Hutt -lom darth qrygg jabba jar hallotan. Bib lars darth gamorrean aruzan ybith.'
-];
-
-function makeStatement(debateId, level) {
-  var statement = { 
-    id: fixtures.length + 1,
-    debate: debateId,
-    responses: [],
-    score: Math.floor(Math.random()*1000), 
-    opposition: Math.floor(Math.random()*1000),
-    support: Math.floor(Math.random()*1000),
-    objection: Math.floor(Math.random()*10),
-    body: statements[Math.floor(Math.random()*statements.length)]
-  };
-
-  fixtures.push(statement);
-
-  if ((level || 0) < 4) {
-    for(var i=0; i<Math.random()*4; i++) {
-      var response = makeStatement(debateId, (level || 0) + 1);
-      response.parent = statement.id;
-      statement.responses.push(response.id);
-    }
-  }
-
-  return statement;
-}
-
-for(var i=0; i<Debate.FIXTURES.length; i++) {
-  for(var j=0; j<Math.random()*5; j++) {
-    var statement = makeStatement(Debate.FIXTURES[i].id);
-    Debate.FIXTURES[i].statements.push(statement.id);
-  }
-}
-
-Statement.reopenClass({
-  FIXTURES: fixtures
-});
-
-export default Statement;
