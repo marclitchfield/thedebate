3abf3a87301a3b4abab66c182a1e52d10b1464fe
marclitchfield <marclitchfield@gmail.com>
2014-11-02 18:30:27 -0800

    Run protractor directly until I can figure out how to get it to run through npm and gulp

--- tests/protractor.conf.js
+++ tests/protractor.conf.js
@@ -2,7 +2,6 @@ var config = require('node-prefix');
 
 exports.config = {
   //seleniumAddress: 'http://localhost:4444/wd/hub',
-  seleniumServerJar: process.env.npm_config_prefix + '/node_modules/protractor/selenium/selenium-server-standalone-2.43.1.jar',
   
   specs: ['acceptance/*.spec.js'],
 
